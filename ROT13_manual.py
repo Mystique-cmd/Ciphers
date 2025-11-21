600435c9efb9780bae9c107845e2c4b3f42f7026
Mystique Soul <mystique@Black-Hat.Black-Hat>
2025-11-21 09:14:19 +0300

    Manual implementation of the ROT13 cipher

---
+++ ROT13_manual.py
@@ -0,0 +1,15 @@
+def rot13(text: str) -> str:
+    result = []
+    for char in text:
+        if 'a' <= char <= 'z':
+            result.append(chr((ord(char) - ord('a') + 13) % 26 + ord('a')))
+        elif 'A' <= char <= 'Z':
+            result.append(chr((ord(char) - ord('A') + 13) % 26 + ord('A')))
+        else:
+            result.append(char)
+    return ''.join(result)
+
+message = input("Enter a message to encode/decode with ROT13: ")
+encoded = rot13(message)
+print("Encoded/Decoded message:", encoded)
+
